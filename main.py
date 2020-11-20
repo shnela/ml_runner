250e76a8abf698e88cde4c68514797be912655f3
Jakub Kuszneruk <>
2020-11-20 23:06:27 +0100

    Create User form

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, ValidationError
+from wtforms.validators import DataRequired, Email
 
 current_dir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
@@ -30,15 +30,26 @@ class User(db.Model):
 
 class MyForm(FlaskForm):
     name = StringField('name', validators=[DataRequired()])
+    email = StringField('email', validators=[DataRequired(), Email()])
     submit = SubmitField('Submit')
+
+    def validate_name(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('Field name should be unique')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Field email should be unique')
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = MyForm()
     if form.validate_on_submit():
-        flash(f"You've just set name to {form.data['name']}")
-        session['name'] = form.data['name']
+        flash(f"You've just created {form.data['name']}")
+        new_user = User(username=form.data['name'], email=form.data['email'])
+        db.session.add(new_user)
+        db.session.commit()
         return redirect(url_for('index'))
     users = User.query.all()
     return render_template('index.html', form=form, users=users)
