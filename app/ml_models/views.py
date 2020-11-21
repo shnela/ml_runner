9ef9386b80dd3da4da0a4db54f0586c6f93ea23a
Jakub Kuszneruk <jkuszneruk@protonmail.com>
2020-11-22 00:15:23 +0100

    Display models_list

--- app/ml_models/views.py
+++ app/ml_models/views.py
@@ -1,1 +1,12 @@
+from flask import render_template
+from flask_login import login_required
+
+from . import bp
 from .models import MLModel
+
+
+@bp.route('/models/')
+@login_required
+def models_list():
+    models = MLModel.query.all()
+    return render_template('ml_models/models_list.html', models=models)
