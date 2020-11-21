10ee0ef4208e4a1218901459ef559b67be40a768
Jakub Kuszneruk <>
2020-11-21 16:28:24 +0100

    Large file structure - separate auth blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -19,7 +19,9 @@ mail = Mail(app)
 migrate = Migrate(app, db)
 
 from app.main import bp as main_bp
+from app.auth import bp as auth_bp
 app.register_blueprint(main_bp)
+app.register_blueprint(auth_bp)
 
 # @app.shell_context_processor
 # def shell_context():
---
+++ app/auth/views.py
@@ -0,0 +1,26 @@
+from flask import render_template, flash, redirect, url_for
+from werkzeug.security import generate_password_hash
+
+from . import bp
+from .forms import CreateUser
+from .models import User
+from .. import db
+from ..utils import send_email
+
+
+@bp.route('/admin/', methods=['GET', 'POST'])
+def admin():
+    form = CreateUser()
+    if form.validate_on_submit():
+        flash(f"You've just created {form.data['name']}")
+        new_user = User(
+            username=form.data['name'],
+            email=form.data['email'],
+            password_hash=generate_password_hash(form.data['password1']),
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        send_email(to=new_user.email, subject='Account created', template_name='email/new_user',
+                   username=new_user.username)
+        return redirect(url_for('main.index'))
+    return render_template('admin.html', form=form)
