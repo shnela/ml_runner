14434cf6edce9bc4b37c101b95ee89e7482b6003
Jakub Kuszneruk <jkuszneruk@protonmail.com>
2020-11-21 22:41:20 +0100

    admin_required with wraps

--- app/auth/utils.py
+++ app/auth/utils.py
@@ -1,8 +1,11 @@
+from functools import wraps
+
 from flask_login import current_user
 from flask import abort
 
 
 def admin_required(f):
+    @wraps(f)
     def inner_func(*args, **kwargs):
         if not current_user.is_admin:
             abort(403)
