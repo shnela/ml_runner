2ec3d000eb0112018a61e8c8d80ce0c5fae82abc
Jakub Kuszneruk <jkuszneruk@protonmail.com>
2020-11-21 23:45:29 +0100

    MLModel created with empty view

---
+++ app/ml_models/models.py
@@ -0,0 +1,11 @@
+from app import db
+
+
+class MLModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    model_name = db.Column(db.String(120), unique=True, nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    pickle_path = db.Column(db.String(256), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'<MLModel {self.model_name}>'
