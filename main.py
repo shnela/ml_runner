8fba4e15eaff2c42d55fbaa8e803237765d7a227
Jakub Kuszneruk <>
2020-11-21 11:18:39 +0100

    send_email function and email templates

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 
 from flask import Flask, render_template, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
-from flask_mail import Mail
+from flask_mail import Mail, Message
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
@@ -19,7 +19,7 @@ app.config['MAIL_PORT'] = '587'
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
-app.config['MAIL_DEFAULT_SENDER'] = 'ML Runner <foo@bar.com>'
+app.config['MAIL_DEFAULT_SENDER'] = f"ML Runner <{ app.config['MAIL_USERNAME'] }>"
 
 Bootstrap(app)
 Moment(app)
@@ -50,9 +50,16 @@ class CreateUser(FlaskForm):
             raise ValidationError('Field email should be unique')
 
 
+def send_email(to, subject, template_name, **kwargs):
+    msg = Message(subject, sender=app.config.get('MAIL_DEFAULT_SENDER'), recipients=[to])
+    msg.body = render_template(f'{template_name}.txt', **kwargs)
+    msg.html = render_template(f'{template_name}.html', **kwargs)
+    mail.send(msg)
+
+
 @app.shell_context_processor
 def shell_context():
-    return dict(User=User, db=db, mail=mail)
+    return dict(User=User, db=db, mail=mail, send_email=send_email)
 
 
 @app.route('/')
