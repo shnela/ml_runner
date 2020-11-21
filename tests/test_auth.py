f38a8b44200c60066f2b729698da2954ae6ac37b
Jakub Kuszneruk <jkuszneruk@protonmail.com>
2020-11-21 21:26:55 +0100

    Test missing name in form

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -2,7 +2,7 @@ from app.auth.models import User
 from tests.conftest import FlaskBaseTest
 
 
-class TestAuth(FlaskBaseTest):
+class TestCreateUser(FlaskBaseTest):
     def test_create_user(self):
         response = self.client.post('/admin/', data={
             'name': 'username',
@@ -14,3 +14,14 @@ class TestAuth(FlaskBaseTest):
 
         assert User.query.count() == 1
         assert User.query.first().email == 'foo@bar.baz'
+
+    def test_create_user_missing_name(self):
+        response = self.client.post('/admin/', data={
+            'email': 'foo@bar.baz',
+            'password1': 'one',
+            'password2': 'one',
+        })
+        assert response.status_code == 200
+
+        assert User.query.count() == 0
+        assert 'This field is required.' in response.data.decode()
