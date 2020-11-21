5e5b8d44c38a66c411980f10fbca0ad6d35072d8
Jakub Kuszneruk <>
2020-11-21 10:44:57 +0100

    Base email config

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 
 from flask import Flask, render_template, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
+from flask_mail import Mail
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
@@ -13,10 +14,17 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(current_dir, 'test.db')}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['MAIL_SERVER'] = 'smtp.mail.yahoo.com'
+app.config['MAIL_PORT'] = '587'
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['MAIL_DEFAULT_SENDER'] = 'ML Runner <foo@bar.com>'
 
 Bootstrap(app)
 Moment(app)
 db = SQLAlchemy(app)
+mail = Mail(app)
 
 
 class User(db.Model):
@@ -44,7 +52,7 @@ class CreateUser(FlaskForm):
 
 @app.shell_context_processor
 def shell_context():
-    return dict(User=User, db=db)
+    return dict(User=User, db=db, mail=mail)
 
 
 @app.route('/')
