2e8909bdf86d20bbdc4a7ce1e03ebcd3e8d925ad
Jakub Kuszneruk <jkuszneruk@protonmail.com>
2020-11-22 00:01:09 +0100

    fake model creator

--- app/fake.py
+++ app/fake.py
@@ -2,6 +2,7 @@ from faker import Faker
 
 from . import db
 from .auth.models import User
+from .ml_models.models import MLModel
 
 fake = Faker()
 
@@ -12,6 +13,18 @@ def generate_users(count=10):
             username=fake.first_name(),
             email=fake.email(),
             password_hash='a',
+        )
+        db.session.add(u)
+    db.session.commit()
+
+
+def generate_ml_models(count=1):
+    for i in range(count):
+        model_name = f'{fake.name()} model'
+        u = MLModel(
+            model_name=model_name,
+            description=fake.text(),
+            pickle_path=f"/some/path/{model_name.replace(' ', '_')}",
         )
         db.session.add(u)
     db.session.commit()
