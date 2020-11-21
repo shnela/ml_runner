47df129542b60e39ebbe993a0982f1a256e115c8
Jakub Kuszneruk <jkuszneruk@protonmail.com>
2020-11-21 22:26:22 +0100

    Is admin created and migrated (new admin set)

--- app/auth/models.py
+++ app/auth/models.py
@@ -8,6 +8,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(120), nullable=True)
+    is_admin = db.Column(db.Boolean, nullable=True)
 
     def __repr__(self):
         return '<User %r>' % self.username
