dacb16870e0e03fe262e5ce8a51eefebc6074479
Jakub Kuszneruk <jkuszneruk@protonmail.com>
2020-11-22 02:59:53 +0100

    Created MLModelRun model with one-to-many relation

--- app/ml_models/models.py
+++ app/ml_models/models.py
@@ -1,4 +1,5 @@
 from app import db
+from datetime import datetime
 
 
 class MLModel(db.Model):
@@ -6,6 +7,18 @@ class MLModel(db.Model):
     model_name = db.Column(db.String(120), unique=True, nullable=False)
     description = db.Column(db.Text, nullable=True)
     pickle_filename = db.Column(db.String(256), unique=True, nullable=False)
+    runs = db.relationship('MLModelRun', backref='ml_model', lazy=True)
 
     def __repr__(self):
         return f'<MLModel {self.model_name}>'
+
+
+class MLModelRun(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    description = db.Column(db.Text, nullable=True)
+    csv_filename = db.Column(db.String(256), unique=True, nullable=False)
+    ml_model_id = db.Column(db.Integer, db.ForeignKey('ml_model.id'))
+
+    def __repr__(self):
+        return f'<MLModelRun {self.model_name} ({self.created_at})>'
