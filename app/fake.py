3e3ac689631ab0e9d1538e0c06082451a6526c22
Jakub Kuszneruk <jkuszneruk@protonmail.com>
2020-11-22 03:26:48 +0100

    Fake generate_ml_models can generate runs now

--- app/fake.py
+++ app/fake.py
@@ -19,21 +19,22 @@ def generate_users(count=10):
     db.session.commit()
 
 
-def generate_ml_models(count=1):
-    for i in range(count):
-        model_name = f'{fake.name()} model'
+def generate_ml_models(models_count=1, runs_count=0):
+    for i in range(models_count):
+        model_name = f'{fake.unique.name()} model'
         u = MLModel(
             model_name=model_name,
             description=fake.text(),
             pickle_filename=f"/some/path/{model_name.replace(' ', '_')}",
         )
         db.session.add(u)
-    db.session.commit()
+        db.session.commit()
+        generate_ml_model_runs(u.id, runs_count)
 
 
-def generate_ml_model_runs(ml_model_id, count=10):
-    for i in range(count):
-        model_name = f'{fake.name()} model'
+def generate_ml_model_runs(ml_model_id, runs_count=10):
+    for i in range(runs_count):
+        model_name = f'{fake.unique.name()} model'
         u = MLModelRun(
             description=fake.text(),
             csv_filename=f"/some/path/{model_name.replace(' ', '_')}",
