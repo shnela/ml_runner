c58e322b85c16ab1398a3bf8de9a918cba065ccd
Jakub Kuszneruk <jkuszneruk@protonmail.com>
2020-11-23 18:35:32 +0100

    Dynamic routes - solution vol. 1
    * additional views implemented

--- main.py
+++ main.py
@@ -9,8 +9,24 @@ def index():
 
 
 @app.route('/user/<name>/')
+@app.route('/other_link/<name>/')
 def hello_from_kwargs(name):
     return f'<h1>Hello string, {name}!</h1>'
+
+
+@app.route('/user/<int:number>/')
+def hello_number(number: int):
+    return f'<h1>Hello number, {number}!</h1>'
+
+
+@app.route('/user/<uuid:uuid>/')
+def hello_uuid(uuid):
+    return f'<h1>Hello uuid, {uuid}!</h1>'
+
+
+@app.route('/user/<path:text>/<int:number>/')
+def hello_both(text, number):
+    return f'<h1>Hello, {text} ({number})!</h1>'
 
 
 if __name__ == '__main__':
