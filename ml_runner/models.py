8645e7127c208b9db1e4d29537efd5cc6b5ed543
Jakub Kuszneruk <jkuszneruk@protonmail.com>
2020-11-25 23:53:45 +0100

    User has written the post - see what happened

--- ml_runner/models.py
+++ ml_runner/models.py
@@ -6,6 +6,7 @@ from ml_runner import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
+    posts = db.relationship('Post', backref='user', lazy=True)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -15,3 +16,4 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.Text(), nullable=False)
     creation_date = db.Column(db.DateTime(), nullable=False, default=datetime.now)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
