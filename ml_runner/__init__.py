a8e843e5bee5497eb558f234130226fb2fc3c37b
Jakub Kuszneruk <jkuszneruk@protonmail.com>
2020-11-25 01:39:02 +0100

    main Blueprint - solution

--- ml_runner/__init__.py
+++ ml_runner/__init__.py
@@ -3,10 +3,12 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 
 from .config import Config
+from .main import bp as main_bp
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 Bootstrap(app)
 
-from ml_runner import views
+# blueprint registration
+app.register_blueprint(main_bp)
