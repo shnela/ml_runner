8b3a5edf6d86c14a5fcb5a2cf6b034e926266cf9
Jakub Kuszneruk <jkuszneruk@protonmail.com>
2020-11-26 08:05:12 +0100

    REST API objects from db - helper

--- ml_runner/api/users.py
+++ ml_runner/api/users.py
@@ -3,27 +3,30 @@ from flask_restful import Resource, fields, marshal_with
 from . import api
 from ..models import User
 
+post_fields = {
+    'content': fields.String,
+    'creation_date': fields.DateTime,
+}
 user_fields = {
     'username': fields.String,
+    'posts': fields.List(fields.Nested(post_fields)),
 }
 user_list_fields = {
     'users': fields.List(fields.Nested(user_fields)),
 }
 
-fake_user = User(username="I'm pretending that I'm from db")  # remove this cheater
-
 
 class UsersEndpoint(Resource):
     @marshal_with(user_fields, envelope='users')
     def get(self, **kwargs):
-        users = [fake_user for _ in range(10)]  # get real user from db
+        users = User.query.all()
         return users
 
 
 class UserEndpoint(Resource):
     @marshal_with(user_fields, envelope='user')
     def get(self, **kwargs):
-        user = fake_user  # get real user from db
+        user = User.query.get_or_404(kwargs['user_id'])
         return user
 
 
