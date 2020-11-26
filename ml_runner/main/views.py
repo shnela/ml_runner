5d939443f5d035a22ac16b0a28dcbf5e6a73de45
Jakub Kuszneruk <jkuszneruk@protonmail.com>
2020-11-27 00:11:25 +0100

    Work with external database schema - solution

--- ml_runner/main/views.py
+++ ml_runner/main/views.py
@@ -2,7 +2,6 @@ from flask import render_template
 
 from . import bp
 from .. import db
-from ..models import User, Post
 from ..reflected_models import ReflectedUser, ReflectedPost
 
 
@@ -13,27 +12,23 @@ def index():
 
 @bp.route('/users/')
 def users_list():
-    users = User.query.all()
-    # users = db.session.query(ReflectedUser).all()
+    users = db.session.query(ReflectedUser).all()
     return render_template('users_list.html', users=users)
 
 
 @bp.route('/users/<int:user_id>/')
 def user_details(user_id):
-    user = User.query.get_or_404(user_id)
-    # user = db.session.query(ReflectedUser).get_or_404(user_id)
+    user = db.session.query(ReflectedUser).get_or_404(user_id)
     return render_template('user_details.html', user=user)
 
 
 @bp.route('/posts/')
 def posts_list():
-    posts = Post.query.all()
-    # posts = db.session.query(ReflectedPost).all()
+    posts = db.session.query(ReflectedPost).all()
     return render_template('posts_list.html', posts=posts)
 
 
 @bp.route('/posts/<int:post_id>/')
 def post_details(post_id):
-    post = Post.query.get_or_404(post_id)
-    # post = db.session.query(ReflectedPost).get_or_404(post_id)
+    post = db.session.query(ReflectedPost).get_or_404(post_id)
     return render_template('post_details.html', post=post)
--- ml_runner/reflected_models.py
+++ ml_runner/reflected_models.py
@@ -14,13 +14,11 @@ if os.environ.get('DO_NOT_REFLECT'):
 
 else:
     # when generating db, reflecting db will cause errors
-
-    # class ReflectedUser(Base):
-    #     __tablename__ = 'user'
-    #     posts = relationship("post", collection_class=list, backref='user')
+    class ReflectedUser(Base):
+        __tablename__ = 'user'
+        posts = relationship("post", collection_class=list, backref='user')
 
     engine = db.engine
     Base.prepare(engine, reflect=True)
 
-    ReflectedUser = Base.classes.user
     ReflectedPost = Base.classes.post
