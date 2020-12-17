5d8fdc69201b2e8e4191b04b8687cb6361f40c49
Jakub Kuszneruk <jkuszneruk@protonmail.com>
2020-12-17 01:46:25 +0100

    REST API - Serializing models - solution

--- ml_runner/api/sms.py
+++ ml_runner/api/sms.py
@@ -5,18 +5,30 @@ from . import api
 from .. import db
 from ..reflected_models import ReflectedShortMessageService
 
+user_base_mfields = {
+    'first_name': fields.String,
+    'last_name': fields.String,
+}
+
 sms_mfields = {
+    'id': fields.Integer,
+    'content': fields.String,
+    'send_date': fields.DateTime,
+    'sending_party': fields.Nested(user_base_mfields),
+    'sent_party': fields.Nested(user_base_mfields),
 }
 
 
 class SMSsEndpoint(Resource):
+    @marshal_with(sms_mfields, envelope='messages')
     def get(self):
-        pass
+        return db.session.query(ReflectedShortMessageService).all()
 
 
 class SMSEndpoint(Resource):
+    @marshal_with(sms_mfields, envelope='message')
     def get(self, sms_id):
-        pass
+        return db.session.query(ReflectedShortMessageService).get_or_404(sms_id)
 
 
 api.add_resource(SMSsEndpoint, '/sms/')
